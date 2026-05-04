f1d59d1dcd43e176878741309fd9fba7c56e1bdc
unknown <ishomnabil@gmail.com>
2026-05-04 15:28:00 +0700

    15th BoneFire new SupaBase

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // 1. URL Project (Tanpa /rest/v1/ di ujungnya)
-const supabaseUrl = 'https://nyqpsgwxtddikxmznvip.supabase.co/rest/v1/' 
+const supabaseUrl = 'https://nyqpsgwxtddikxmznvip.supabase.co' 
 
 // 2. Anon Key yang baru aja lu kirim
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55cXBzZ3d4dGRkaWt4bXpudmlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4NzUxNTAsImV4cCI6MjA5MzQ1MTE1MH0.lm-1vXKVStFNu84vduO9c_M9f5sZzucXiRReLUXGukk'
